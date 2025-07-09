cc858a510f16ffe28b751b111d70a73abc2657c3
Anthony Schurle <115958429+UnveiledSafe8@users.noreply.github.com>
2025-07-09 11:46:51 -0500

    Allow setting postgres container name

--- app/backend/database/database.py
+++ app/backend/database/database.py
@@ -5,8 +5,9 @@ import os
 
 SQL_PASSWORD = os.getenv("SQL_PASSWORD")
 SQL_USERNAME = os.getenv("SQL_USERNAME")
+POSTGRES_CONTAINER = os.getenv("POSTGRES_CONTAINER")
 
-engine = create_engine(f"postgresql+psycopg2://{SQL_USERNAME}:{SQL_PASSWORD}@postgres:5432/signal_siege_database", echo=True)
+engine = create_engine(f"postgresql+psycopg2://{SQL_USERNAME}:{SQL_PASSWORD}@{POSTGRES_CONTAINER}:5432/signal_siege_database", echo=True)
 
 Base = declarative_base()
 
